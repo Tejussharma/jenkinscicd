8a370e7c24ab4a14669b7c00011e9e95694e46d6
tejus <tejussharma5252@gmail.com>
2025-05-08 00:32:39 +0530

    first commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,25 @@
+import { test,expect } from '@playwright/test';
+
+test('Test 1 @dev' , async({page})=>{
+await page.goto("https://saucedemo.com");
+await expect(page).toHaveTitle('Swag Labs');
+})
+
+test('Test 2 @uat' , async({page})=>{
+    await page.goto("https://saucedemo.com");
+    console.log("Reached the Page - 2");
+    await expect(page).toHaveTitle('Swag Labs');
+})
+test('Test 3 @dev' , async({page})=>{
+    await page.goto("https://saucedemo.com");
+    await expect(page).toHaveTitle('Swag Labs');
+    })
+    
+    test('Test 4 @uat' , async({page})=>{
+        await page.goto("https://saucedemo.com");
+        console.log("Reached the Page - 2");
+        await expect(page).toHaveTitle('Swag Labs');
+    })
+        
+    
+
